d083fb3904bba206e3c658ffef2d5746ffc09819
Akash Adhikari <akash_532998@yahoo.com>
2018-04-17 11:14:10 +0545

    Heap Sort

--- Merge_Sort.py
+++ Merge_Sort.py
@@ -1,17 +1,22 @@
 def merge_sort(alist):
+
     print("Splitting ",alist)
+
     if len(alist)>1:
+
         mid = len(alist)//2
         left_half = alist[:mid]
         right_half = alist[mid:]
 
+        # recursive call to respective halves
+
         merge_sort(left_half)
         merge_sort(right_half)
 
-        i=0
-        j=0
-        k=0
+        i = j = k = 0
+
         while i < len(left_half) and j < len(right_half):
+
             if left_half[i] < right_half[j]:
                 alist[k]=left_half[i]
                 i=i+1
@@ -21,16 +26,21 @@ def merge_sort(alist):
             k=k+1
 
         while i < len(left_half):
+
             alist[k]=left_half[i]
             i=i+1
             k=k+1
 
         while j < len(right_half):
+
             alist[k]=right_half[j]
             j=j+1
             k=k+1
+
     print("Merging ",alist)
 
 alist = [54,26,93,17,77,31,44,55,20]
+
 merge_sort(alist)
+
 print(alist)
